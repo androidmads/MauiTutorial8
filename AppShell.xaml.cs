569f00f6ca74f1c4e9acd7d827af62d018c2e345
Mushtaq M A <mushtaqat3gb@gmail.com>
2022-11-19 23:42:59 +0530

    Add project files.

---
+++ AppShell.xaml.cs
@@ -0,0 +1,9 @@
+﻿namespace MauiTutorial8;
+
+public partial class AppShell : Shell
+{
+	public AppShell()
+	{
+		InitializeComponent();
+	}
+}
---
+++ MainPage.xaml.cs
@@ -0,0 +1,20 @@
+﻿namespace MauiTutorial8;
+
+public partial class MainPage : ContentPage
+{
+	int count = 0;
+
+	public MainPage()
+	{
+		InitializeComponent();
+		level2.Text = Preferences.Get("savekey", "Default Value");
+    }
+
+	private void OnCounterClicked(object sender, EventArgs e)
+	{
+		Preferences.Set("savekey", "New Value");
+		level2.Text = Preferences.Get("savekey", "Default Value");
+        SemanticScreenReader.Announce(CounterBtn.Text);
+	}
+}
+
